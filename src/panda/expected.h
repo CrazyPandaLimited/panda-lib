2230ea99176ffd1871b747d595e98e4ba0ce9572
syber <devnull@localhost>
2019-05-16 16:39:59 +0300

    expected related

--- src/panda/expected.h
+++ src/panda/expected.h
@@ -4,4 +4,14 @@
 namespace panda {
     template <class T, class E>
     using expected = tl::expected<T,E>;
+
+    template <class E>
+    using unexpected = tl::unexpected<E>;
+
+    template<class E>
+    using bad_expected_access = tl::bad_expected_access<E>;
+
+    using unexpect_t = tl::unexpect_t;
+
+    inline constexpr unexpect_t unexpect{};
 }
