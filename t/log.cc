5098a7c7af2717e6787a03f00faf3e4022b6e006
syber <syber@crazypanda.ru>
2020-05-08 23:56:16 +0300

    logger: formatter upgrade

--- t/log.cc
+++ t/log.cc
@@ -34,6 +34,11 @@ struct Ctx {
     }
 };
 
+static inline void REGCHECK (const string& s, const string& re) {
+    WARN("checking '" << s << "' with regex '" << re <<"'");
+    CHECK(std::regex_search(std::string(s.c_str()), std::regex(std::string(re.c_str()))));
+}
+
 TEST("set_level") {
     Ctx c;
     set_level(VerboseDebug);
@@ -181,21 +186,122 @@ TEST("set_formatter") {
 TEST("set_format") {
     Ctx c;
     Module panda_log_module("epta");
-    set_format("LEVEL=%L FILE=%f LINE=%l FUNC=%F MODULE=%M MESSAGE=%m DATE=%d DATEH=%D TIME=%t TIMEH=%T PID=%p THREAD=%P UNIX=%u UNIXH=%U ");
-    panda_log_alert("mymsg");
-    WARN(c.fstr);
-    auto res = std::regex_search(std::string(c.fstr.c_str()), std::regex(
-        "LEVEL=alert FILE=log.cc LINE=185 FUNC=\\w+ MODULE=epta MESSAGE=mymsg "
-        "DATE=\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} "
-        "DATEH=\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}(\\.\\d+)? "
-        "TIME=\\d{2}:\\d{2}:\\d{2} "
-        "TIMEH=\\d{2}:\\d{2}:\\d{2}(\\.\\d+)? "
-        "PID=\\d+ "
-        "THREAD=\\w+ "
-        "UNIX=\\d+ "
-        "UNIXH=\\d+(\\.\\d+)? "
-    ));
-    CHECK(res);
+
+    SECTION("level") {
+        set_format("LEVEL=%L");
+        panda_log_alert();
+        CHECK(c.fstr == "LEVEL=alert");
+    }
+
+    SECTION("module") {
+        SECTION("default") {
+            set_format("MODULE=%M");
+            panda_log_alert();
+            CHECK(c.fstr == "MODULE=epta");
+        }
+        SECTION("strip") {
+            set_format(" MOD=%4.1M ");
+            panda_log_alert(::panda_log_module, "");
+            CHECK(c.fstr == " ");
+        }
+    }
+
+    SECTION("function") {
+        set_format("FUNC=%F");
+        panda_log_alert();
+        CHECK(c.fstr.find("FUNC=____C_A_T_C_H____T_E_S_T____") == 0);
+    }
+
+    SECTION("file") {
+        SECTION("short name") {
+            set_format("FILE=%f");
+            panda_log_alert();
+            CHECK(c.fstr == "FILE=log.cc");
+        }
+        SECTION("full name") {
+            set_format("FILE=%1f");
+            panda_log_alert();
+            REGCHECK(c.fstr, "FILE=t[\\/]log.cc");
+        }
+    }
+
+    SECTION("line") {
+        set_format("LINE=%l");
+        panda_log_alert();
+        CHECK(c.fstr == "LINE=230");
+    }
+
+    SECTION("message") {
+        set_format("MSG=%m");
+        panda_log_alert("mymsg");
+        CHECK(c.fstr == "MSG=mymsg");
+    }
+
+    SECTION("current time") {
+        string pat = "TIME=";
+        string re;
+        SECTION("Y4 date") {
+            re = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}";
+            SECTION("low-res") {
+                pat += "%t";
+            }
+            SECTION("hi-res") {
+                pat += "%.1t";
+                re += "\\.\\d{1}";
+            }
+        }
+        SECTION("Y2 date") {
+            re = "\\d{2}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}";
+            SECTION("low-res") {
+                pat += "%1t";
+            }
+            SECTION("hi-res") {
+                pat += "%1.2t";
+                re += "\\.\\d{2}";
+            }
+        }
+        SECTION("hms") {
+            re = "\\d{2}:\\d{2}:\\d{2}";
+            SECTION("low-res") {
+                pat += "%2t";
+            }
+            SECTION("hi-res") {
+                pat += "%2.3t";
+                re += "\\.\\d{3}";
+            }
+        }
+        SECTION("unix ts") {
+            re = "\\d+";
+            SECTION("low-res") {
+                pat += "%3t";
+            }
+            SECTION("hi-res") {
+                pat += "%3.9t";
+                re += "\\.\\d{9}";
+            }
+        }
+        set_format(pat);
+        panda_log_alert();
+        REGCHECK(c.fstr, re + '$');
+    }
+
+    SECTION("thread id") {
+        set_format("THREAD=%T");
+        panda_log_alert();
+        REGCHECK(c.fstr, "THREAD=\\d+");
+    }
+
+    SECTION("process id") {
+        set_format("PID=%p");
+        panda_log_alert();
+        REGCHECK(c.fstr, "PID=\\d+");
+    }
+
+    SECTION("process title") {
+        set_format("TITLE=%P");
+        panda_log_alert();
+        REGCHECK(c.fstr, "TITLE=.+");
+    }
 }
 
 TEST("should_log") {
