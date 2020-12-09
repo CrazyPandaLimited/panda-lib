a5cf59381fb3fed2df1e253a6cdfc7138285fed0
syber <syber@crazypanda.ru>
2020-12-09 23:43:52 +0300

    cmake/makefile related clenaup

--- clib/panda-lib-config.cmake
+++ clib/panda-lib-config.cmake
@@ -1,3 +1,3 @@
-if (NOT TARGET panda::panda-lib)
-    include("${CMAKE_CURRENT_LIST_DIR}/panda-libTargets.cmake")
+if (NOT TARGET panda-lib)
+    include("${CMAKE_CURRENT_LIST_DIR}/panda-lib-targets.cmake")
 endif()
