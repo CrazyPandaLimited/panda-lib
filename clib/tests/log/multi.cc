a1decda96da0b4d7ccd0809f7912732c7ed0fcc8
syber <syber@crazypanda.ru>
2021-03-05 17:36:56 +0300

    logger: add supportfor recursive multilogger

--- clib/tests/log/multi.cc
+++ clib/tests/log/multi.cc
@@ -55,3 +55,34 @@ TEST("using different formatters") {
     CHECK(m2 == "F3:hi");
     CHECK(m3 == "F1:hi");
 }
+
+TEST("multi into multi") {
+    Ctx c;
+    int cnt = 0;
+    string s[10];
+    set_formatter("D:%m");
+
+    set_logger(new MultiLogger({
+        { [&](auto& m, auto&) { ++cnt; s[1]=m; } },
+        { [&](auto& m, auto&) { ++cnt; s[2]=m; }, "L2:%m" },
+        { [&](auto& m, auto&) { ++cnt; s[3]=m; }, "L3:%m", Level::Emergency },
+        {
+            new MultiLogger({
+                { [&](auto& m, auto&) { ++cnt; s[4]=m; } },
+                { [&](auto& m, auto&) { ++cnt; s[5]=m; }, "ML2:%m" },
+                { [&](auto& m, auto&) { ++cnt; s[6]=m; }, "ML3:%m", Level::Emergency },
+            }),
+            "M:%m"
+        }
+    }));
+
+    panda_log_alert("hi");
+
+    CHECK(cnt == 4);
+    CHECK(s[1] == "D:hi");
+    CHECK(s[2] == "L2:hi");
+    CHECK(s[3].empty());
+    CHECK(s[4] == "M:hi");
+    CHECK(s[5] == "ML2:hi");
+    CHECK(s[6].empty());
+}
