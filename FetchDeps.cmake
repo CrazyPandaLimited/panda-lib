f07f79a3b29f6fcd0011e72b44db25fd0a4992bd
g.smorkalov <g.smorkalov@crazypanda.ru>
2023-03-21 21:39:47 +0300

    CMake: FetchDeps.cmake fixes

--- FetchDeps.cmake
+++ FetchDeps.cmake
@@ -1,21 +1,9 @@
 include(FetchContent)
 
-set(Catch2_repository https://github.com/catchorg/Catch2.git)
-set(Catch2_repository_tag devel)
-
 if (${PANDALIB_TESTS})
-    set(deps Catch2)
+    FetchContent_Declare(Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG devel
+    )
+    FetchContent_MakeAvailable(Catch2)
 endif()
-
-foreach(dep ${deps})
-    if (NOT ${dep}_POPULATED)
-        if (NOT DEFINED ${${dep}_repository_tag})
-            set (${${dep}_repository_tag} master)
-        endif()
-        FetchContent_Declare(${dep}
-            GIT_REPOSITORY ${${dep}_repository}
-            GIT_TAG ${${dep}_repository_tag}
-        )
-        FetchContent_MakeAvailable(${dep})
-    endif ()
-endforeach()
